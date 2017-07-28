6dcc4f70dff124f6372b82dd730c4bc27fe7ab85
Amir Elemam <amir.elemam@usp.br>
2017-07-28 04:32:07 -0300

    Descrição da apresentação, licença, requirements e script

---
+++ README.md
@@ -0,0 +1,12 @@
+[Clique aqui para ver a versão em Português](README_PT.md)
+
+## Chatbot with Python
+
+This code referes to the presentation "Chatbot with Python", for SciPy #1 on 29/07/2017.
+Slides available [here](https://www.slideshare.net/AmirdoNascimentoElem/chatbots-em-python?utm_source=slideshow&utm_medium=ssemail&utm_campaign=post_upload_view_cta)(Portuguese).
+
+### Setting up 
+
+### License
+
+This project is licensed under the [MIT License](LICENSE)
---
+++ chatbot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import luis
+
+LUIS_ENDPOINT = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9fcf2e84-fef8-46e1-b4ee-992cd024c888?subscription-key=94c91f8f3a0d46d6bb516227bd208728&verbose=true&timezoneOffset=0&q="
+
+LUIS = luis.Luis(url=LUIS_ENDPOINT)
+
+
+class Main(object):
+    """Baseado na pergunta do usuário, mostra a resposta mais adequada"""
+
+    ANSWERS = {
+        "BEST_LANGUAGE": "Sem sombra de dúvidas, a melhor linguagem de programação é Python.",
+        "WORST_LANGUAGE": "Não sei, mas com a mais absoluta certeza, não é Python.",
+        "ABOUT_PYTHON": "Python é a melhor linguagem de programação do mundo.",
+        "WELCOME": "Oi amigo! Faça sua pergunta...",
+        "GOODBYE": "Tchauzinho! :)",
+        "SENTIDO_VIDA": "O sentido da vida é programar em Python",
+        "None": "Desculpe, não entendi. Por favor, tente escrever de outra forma."
+    }
+
+    def replies(self, text):
+        """Baseado na pergunta do usuário, identifica qual a sua intenção"""
+        intent = LUIS.analyze(text).best_intent().intent
+        answer = self.ANSWERS[intent]
+        return answer
+
+    def start(self):
+        """Initializa o bot"""
+        print("Início da interação:")
+
+        while True:
+            question = input()
+            answer = self.replies(question)
+            print(answer)
+
+
+if __name__ == '__main__':
+    Main().start()
